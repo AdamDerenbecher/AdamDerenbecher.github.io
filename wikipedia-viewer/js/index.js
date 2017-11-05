74c65d4b6d4b25a5b706dbdd39869505ef36d4ec
derenbea <adam.derenbecher@wwt.com>
2017-11-05 16:46:04 -0600

    working with api now

--- wikipedia-viewer/js/index.js
+++ wikipedia-viewer/js/index.js
@@ -1,22 +1,60 @@
 "use strict";
 
 $(document).ready(function () {
-	$("#myForm").focus(function () {
+	$("input").focus(function () {
 		$("#myForm").animate({
 			width: "80%",
-			marginTop: 0
+			border: "solid"
 		}, "slow");
 	});
-	$("input").focus(function () {
+
+	var resultHtml;
+
+	$("#searchButton").click(function f() {
 		$("#myForm").animate({
 			width: "80%",
 			border: "solid",
 			top: "10px"
 		}, "slow");
-	});
-	$("#myForm").submit(function () {
-		$("#resultDiv").html("\n\t\t\t\t\t\t\t\t\t\t\t\t <div class=\"list-group\">\n  <a href=\"#\" class=\"list-group-item list-group-item-action flex-column align-items-start active\">\n    <div class=\"d-flex w-100 justify-content-between\">\n      <h5 class=\"mb-1\">List group item heading</h5>\n      <small>3 days ago</small>\n    </div>\n    <p class=\"mb-1\">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>\n    <small>Donec id elit non mi porta.</small>\n  </a>\n  <a href=\"#\" class=\"list-group-item list-group-item-action flex-column align-items-start\">\n    <div class=\"d-flex w-100 justify-content-between\">\n      <h5 class=\"mb-1\">List group item heading</h5>\n      <small class=\"text-muted\">3 days ago</small>\n    </div>\n    <p class=\"mb-1\">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>\n    <small class=\"text-muted\">Donec id elit non mi porta.</small>\n  </a>\n  <a href=\"#\" class=\"list-group-item list-group-item-action flex-column align-items-start\">\n    <div class=\"d-flex w-100 justify-content-between\">\n      <h5 class=\"mb-1\">List group item heading</h5>\n      <small class=\"text-muted\">3 days ago</small>\n    </div>\n    <p class=\"mb-1\">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>\n    <small class=\"text-muted\">Donec id elit non mi porta.</small>\n  </a>\n</div>\n\t\t\t\t\t\t\t\t\t\t\t\t ");
-		$("#resultDiv").slideDown("slow");
+		$.ajax({
+			url: "https://en.wikipedia.org/w/api.php",
+			data: {
+				format: "json",
+				action: "opensearch",
+				search: $("#searchBox").val(),
+				namespace: 0,
+				limit: 10
+			},
+			dataType: "jsonp",
+			success: function success(data) {
+				var resultCount = data[1].length;
+				var searchTerm = data[0];
+
+				resultHtml = "<div class=\"list-group\">";
+
+				var i;
+				for (i = 1; i < resultCount; i++) {
+					var resultTitle = data[1][i];
+					var resultDesc = data[2][i];
+					var resultLink = data[3][i];
+
+					var oneResult = "\n\t\t\t\t\t\t  <a href=\"" + resultLink + "\" target=\"_blank\" class=\"list-group-item list-group-item-action flex-column align-items-start\">\n\t\t\t\t\t\t\t\t<div class=\"d-flex w-100 justify-content-between\">\n\t\t\t\t\t\t\t\t\t<h5 class=\"mb-1\">" + resultTitle + "</h5>\n\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t<p class=\"mb-1\">" + resultDesc + "</p>\n\t\t\t\t\t\t\t</a>\n\t\t\t\t\t";
+
+					resultHtml += oneResult;
+				}
+
+				resultHtml += "</div>";
+				$("#myForm").animate({
+					width: "80%",
+					marginTop: 0
+				}, "slow");
+
+				$("#resultDiv").html("" + resultHtml);
+				$(".pre-scrollable").css("max-height", "80%");
+				$("#resultDiv").slideDown("slow");
+			}
+		});
+
 		return false;
 	});
 });
